86894ffdd922da68212384a940c927f23ae80798
a-stepanchikov <aleksrims@gmail.com>
2024-01-13 21:04:45 +0300

    complete code lesseon02

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-const title = 1;
-const screens = 2;
-const screenPrice = 3;
-const rollback = 4;
-const fullPrice = 5;
-const adaptive = 6;
+let myVar;
 
-alert ('Hello World!');
+myVar = 10; 
+console.log (typeof myVar);
+myVar = 12345678998745621223n;
+console.log (typeof myVar);
+myVar = "Hello World!";
+console.log (typeof myVar);
+myVar = true;
+console.log (typeof myVar);
+myVar = null;
+console.log (typeof myVar);
+myVar = undefined;
+console.log (typeof myVar);
 
-console.log('Hello World');
+let i = 5
+
+console.log (--i);
+
+const title = "Project";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 11;
+const rollback = 10;
+const fullPrice = 1000000;
+const adaptive = true;
+
+console.log (typeof title);
+console.log (typeof fullPrice);
+console.log (typeof adaptive);
+console.log (screens.length);
+console.log ("Стоимость верстки экранов" + " " + screenPrice + " $");
+console.log ("Стоимость разработки сайта" + " " + fullPrice + " рублей");
+console.log (screens.toLowerCase().split(", "));
+
+let percent = "Процент отката посреднику за работу" + " " + fullPrice * (rollback / 100) + " рублей";
+
+console.log (percent);
+
+
+
+
+
+
