1e534f9415bb75d53e02a117dbf291568776bbb3
a-stepanchikov <aleksrims@gmail.com>
2024-01-17 23:11:30 +0300

    comlete code lesson03

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
-const title = "Project";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 11;
-const rollback = 10;
-const fullPrice = 1000000;
-const adaptive = true;
+'use strict';
 
-console.log (typeof title);
-console.log (typeof fullPrice);
-console.log (typeof adaptive);
-console.log (screens.length);
-console.log ("Стоимость верстки экранов" + " " + screenPrice + " $");
-console.log ("Стоимость разработки сайта" + " " + fullPrice + " рублей");
-console.log (screens.toLowerCase().split(", "));
+let answerTitle = prompt('Как называется ваш проект?');
+let answerScreens = prompt('Какие типы экранов нужно разработать?');
+let answerScreenPrice = prompt('Сколько будет стоить данная работа?');
+let answerAdaptive = prompt('Нужен ли адаптив на сайте?');
 
-let percent = "Процент отката посреднику за работу" + " " + fullPrice * (rollback / 100) + " рублей";
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоить?');
 
-console.log (percent);
+
+
+let title = answerTitle;
+const screens = answerScreens;
+const screenPrice = Number(answerScreenPrice);
+const adaptive = !!answerAdaptive;
+
+let answerService1 = service1;
+let answerServicePrice1 = Number(servicePrice1);
+let answerService2 = service2;
+let answerServicePrice2 = Number(servicePrice2);
+
+let fullPrice = screenPrice + answerServicePrice1 + answerServicePrice2;
+
+const rollback = 5;
+let percent = fullPrice * (rollback / 100);
+
+let servicePercentPrice = Math.ceil(fullPrice - percent);
+
+
+console.log (title);
+console.log (screens);
+console.log (screenPrice);
+console.log (adaptive);
+console.log (fullPrice);
+console.log (servicePercentPrice);
+
+  switch (true) {
+      case fullPrice > 30000 || fullPrice == 30000:
+          console.log ("Даем скидку в 10%");
+          break
+      case fullPrice > 15000 || fullPrice == 15000 && fullPrice < 30000:
+          console.log ("Даем скидку в 5%");
+          break
+      case fullPrice < 15000 && fullPrice > 0 || fullPrice == 0:
+          console.log ("Скидка не предусмотрена");
+          break
+      case fullPrice < 0:
+          console.log ("Что то пошло не так");
+          break
+  }
+
+
+
+
+
 
 
 
